6bc4cca962ab2138416eb3f5be4cfdae4e57619a
sowahtech <sowahtech@gmail.com>
2024-01-16 22:57:49 +0000

    completed functionality for sign up page

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const userRouter = require("./routes/user.route.js");
 const authRouter = require("./routes/auth.route.js");
+const cors = require("cors");
 
 mongoose
   .connect(process.env.MONGO)
@@ -15,10 +16,23 @@ mongoose
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.listen(5000, () => {
   console.log("server started successfully on port 5000 and i say hurray");
+});
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, DELETE");
+
+  next();
 });
 
 app.use("/api/user", userRouter);
