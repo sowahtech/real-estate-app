87567534fc3a0f13cec7212631418d413f342b92
sowahtech <sowahtech@gmail.com>
2024-01-19 18:39:11 +0000

    created sign in API route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const signin = require("../controllers/auth.controller.js");
 const signup = require("../controllers/auth.controller.js");
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 module.exports = router;
